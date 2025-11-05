2a9f4b2d0cf565807a0514064146908fbdc0fe63
Chris <cl.kram+itb36-github@posteo.de>
2025-11-05 13:19:00 +0100

    Initialize WPF project

---
+++ GanttProgram/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace GanttProgram
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
